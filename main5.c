fc519816940683f9f17e20d40eaf8aa1e3a5dd1b
se4019 <70657061+se4019@users.noreply.github.com>
2020-11-05 16:22:14 +0900

    10_05

---
+++ main5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i ;
+	int grade[5];
+	int sum=0;
+	
+	
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i] =", i);
+		scanf("%d", &grade[i]);
+	}
+	
+	for (i=0;i<5;i++)
+	{
+		sum += grade[i];
+		printf("grade[%d] = %d\n", i, grade[i]);
+	}
+	
+	printf("average : %i\n", sum/5);
+	
+	
+	return 0;
+}
